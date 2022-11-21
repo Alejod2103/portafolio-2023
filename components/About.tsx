01b28aeac3f5ae2d86c1e653fe03517571b50d56
alejodev2103 <alejod2103@gmail.com>
2022-11-21 16:02:38 +0000

    commit 1.7

--- components/About.tsx
+++ components/About.tsx
@@ -35,7 +35,7 @@ function About({}: Props) {
       />
 
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold">
+        <h4 className="text-4xl mt-4 font-semibold">
           Que es lo que {" "}
           <span className="underline decoration-[#80D133]/50">hago? </span>{" "}
         </h4>
