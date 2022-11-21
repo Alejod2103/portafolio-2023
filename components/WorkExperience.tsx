63e34f5bf07a9d6d86e752fe55f8b6a7d3f93e7b
alejodev2103 <alejod2103@gmail.com>
2022-11-21 16:28:06 +0000

    correccion seccion experiencias

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -19,7 +19,7 @@ function WorkExperience({}: Props) {
     className='h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-20 uppercase tracking-[15px] text-gray-500 text-2xl' >Experiencias</h3>
 
-        <div className='w-full flex space-x-5 overflow-x-scroll p-20 snap-x snap-mandatory' >
+        <div className='w-full h-[800px] flex space-x-5 overflow-x-scroll p-20 snap-x snap-mandatory' >
             <ExpCard />
             <ExpCard />
             <ExpCard />
