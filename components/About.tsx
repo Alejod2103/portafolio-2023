d88036b1c51ad5cbb15be7641e749e363db64f0a
alejodev2103 <alejod2103@gmail.com>
2022-11-21 15:58:47 +0000

    commit 1.6

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ function About({}: Props) {
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly
     mx-auto items-center"
     >
-      <h3 className="absolute top-28 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         sobre mi
       </h3>
 
