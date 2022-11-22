c83325830e29aa6834e7f2b34ae4e42fdf75e982
alejodev2103 <alejod2103@gmail.com>
2022-11-22 15:41:46 +0000

    agregando seccion proyectos

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 function Projects({}: Props) {
   const proyectos = ["1", "2", "3", "4", "5"];
     return (
-    <div className='relative h-screen flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0' >
+    <motion.div 
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1.5 }}
+    className='relative h-screen flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0' >
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Proyectos</h3>
         
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20' >
           {proyectos.map((proyecto, i) => (
-            <div key={i} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen ' ><img 
+            <div key={i} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen ' >
+            <motion.img
+              initial={{
+                y: -300,
+                opacity: 0
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once:true }}
             src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png" 
             alt="none" 
             />
-            <div><h4>Case study {i + 1} of {proyecto.length}: Netflix Clone</h4></div>
+              <div className='space-y-10 px-0 md:px-10 max-w-6xl' >
+                <h4 className='text-4xl font-semibold text-center' >
+                  <span>
+                    Case study {i + 1} of {proyecto.length}:
+                  </span>{" "}
+                  Netflix Clone
+                </h4>
+
+                <p className='text-lg text-center md:text-left' >
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, veniam tempore eos tenetur, aliquam, totam architecto dolores facere accusamus neque ullam eaque doloremque voluptatum facilis voluptates error alias corporis harum!
+                </p>
+              </div>
             </div>
           ))}
 
@@ -22,7 +46,7 @@ function Projects({}: Props) {
 
         <div className="w-full absolute top-[30%] bg-[#80D133]/10 left-0 h-[500px] -skew-y-12" />
 
-    </div>
+    </motion.div>
   )
 }
 
